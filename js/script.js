a62eee31f759da5901d97ce4db486be58090c784
Kamil <kamciu9@gmail.com>
2020-08-06 00:34:55 +0200

    projects and layout updated

---
+++ js/script.js
@@ -0,0 +1,14 @@
+(function () {
+    var windowH = $(window).height(),
+        docEl = $(document),
+        aboutPage = $('#about'),
+        homePage = $('#home'),
+        btn = $('#slide'),
+        animSpeed = 500;
+
+    $('#slide').click((e) => {
+        aboutPage.show();
+
+    })
+
+})();
